31d68f204375433f3cc68917fcd8477edca2421e
ejrkdgo <ejrkdgoz@gmail.com>
2022-01-11 14:05:15 +0900

    22011_eslint-sample

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import './App.css';
 
 function App() {
-    for (var i = 0; i < 10; i--) {
-
-    }
-
-    if (foo) {
-    }
-
-    while (foo) {
-    }
-
     return (
         <>
             <div className='App'></div>
